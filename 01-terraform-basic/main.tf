f6f30c0b1b8a3feb4aa3669dbec16f32885cea8c
Junaid Khan <junaid.khan@slalom.com>
2022-01-27 18:29:35 -0500

    base terraform

--- 01-terraform-basic/main.tf
+++ 01-terraform-basic/main.tf
@@ -1,1 +1,14 @@
-none
+terraform {
+    required_providers {
+      aws = {
+          source = "hashicorp/aws"
+          version = "~>3.0"
+      }
+    }
+}
+
+# Configure the AWS provider
+provider "aws" {
+    region = "us-east-1"
+ # Version not meeded 
+}
