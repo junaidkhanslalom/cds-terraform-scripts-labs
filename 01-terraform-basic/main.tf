7a9bfe011ddf09aa943f49d3d419ece84c1572f5
Junaid Khan <junaid.khan@slalom.com>
2022-01-27 20:46:59 -0500

    adding s3 bucket using terraform

--- 01-terraform-basic/main.tf
+++ 01-terraform-basic/main.tf
@@ -12,3 +12,9 @@ provider "aws" {
     region = "us-east-1"
  # Version not meeded 
 }
+
+# plan - execute 
+resource "aws_s3_bucket" "my_s3_bucket" {
+    bucket = "my-s3-bucket-juni-2311"   
+  
+}
