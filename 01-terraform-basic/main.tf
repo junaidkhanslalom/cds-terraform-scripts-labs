b1e67775ee1e17ba91253f24b4c0ebe4c9493d78
Junaid Khan <junaid.khan@slalom.com>
2022-01-27 21:17:23 -0500

    versioning the s3 bucket

--- 01-terraform-basic/main.tf
+++ 01-terraform-basic/main.tf
@@ -15,6 +15,19 @@ provider "aws" {
 
 # plan - execute 
 resource "aws_s3_bucket" "my_s3_bucket" {
-    bucket = "my-s3-bucket-juni-2311"   
+    bucket = "my-s3-bucket-juni-2022-cds" 
+    versioning {
+        enabled = true
+    }  
   
+}
+
+output "my_s3_bucket_versioning" {
+    value=aws_s3_bucket.my_s3_bucket.versioning[0].enabled 
+
+}
+
+output "my_s3_bucket_complete_details" {
+    value=aws_s3_bucket.my_s3_bucket 
+
 }
