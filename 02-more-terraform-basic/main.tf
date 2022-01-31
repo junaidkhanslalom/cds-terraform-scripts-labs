815bc7258c6773a96d9517577b4a0468a8a29a17
Junaid Khan <junaid.khan@slalom.com>
2022-01-30 19:45:01 -0500

    understanding variables

---
+++ 02-more-terraform-basic/main.tf
@@ -0,0 +1,30 @@
+
+
+
+variable "iam_user_name_prefix" {
+  type    = string #any, number, bool, list, map, set, object, tuple
+  default = "my_iam_user"
+
+}
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+resource "aws_iam_user" "my_iam_users" {
+  count = 1
+  name  = "${var.iam_user_name_prefix}_${count.index}"
+
+}
