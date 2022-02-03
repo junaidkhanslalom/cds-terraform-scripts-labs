ca9e82304c94ef10fe03bc8b0afa766f518eb87e
Junaid Khan <junaid.khan@slalom.com>
2022-02-02 21:39:57 -0500

    ec2 scripts, removed hardcoding

--- 05-ec2-instances/main.tf
+++ 05-ec2-instances/main.tf
@@ -1,6 +1,4 @@
-variable "aws_key_pair" {
-  default = "~/aws/aws_keys/default-ec2.pem"
-}
+
 
 terraform {
   required_providers {
@@ -23,14 +21,12 @@ provider "aws" {
 //SG
 resource "aws_default_vpc" "default" {
 
-  
+
 }
 
-data "aws_subnet_ids" "default_subnets" {
-  vpc_id = aws_default_vpc.default.id
-}
+
 resource "aws_security_group" "http_server_sg" {
-  name   = "http_server_sg"
+  name = "http_server_sg"
   // vpc_id = "vpc-0b10a5bb75da36afd"
   vpc_id = aws_default_vpc.default.id
 
@@ -62,7 +58,8 @@ resource "aws_security_group" "http_server_sg" {
 }
 
 resource "aws_instance" "http_server" {
-  ami                    = "ami-0a8b4cd432b1c3063"
+  # ami                    = "ami-0a8b4cd432b1c3063"
+  ami                    = data.aws_ami.aws-linux-2-latest
   key_name               = "default-ec2"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.http_server_sg.id]
@@ -77,7 +74,7 @@ resource "aws_instance" "http_server" {
     private_key = file(var.aws_key_pair)
   }
 
-provisioner "remote-exec" {
+  provisioner "remote-exec" {
     inline = [
       "sudo yum install httpd -y",
       "sudo service httpd start",
